7c61b1d748f778d2efb22288e90f56a778d0a7ff
Juan <juanjpnv@hotmail.com>
2019-12-15 23:04:18 -0300

    Adicionado função para gerar NPZ apartir do main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 from classificar import Reconhecer
 from feature_extractor import ExtairCaracteristicas
+import detect
 
 #%%
 def recortar(x1, y1, w, h, img):
@@ -16,6 +17,15 @@ def recortar(x1, y1, w, h, img):
     cara = img[Y1:Y2, X1:X2] # recorta o rosto
     cv2.rectangle(img, (X1, Y1), (X2, Y2), (255, 0 , 0))
     return cara
+
+def novo_NPZ_de_features(path, nome):
+    '''
+        Ler um CSV com fotos, extrai os rostos e as caracteristicas.
+        Gera um novos NPZ na pasta dataset
+    '''
+    detect.preparar_e_salvar_dataset(path, f'resultados/{nome}.npz')
+    EC = ExtairCaracteristicas()
+    EC.criar_feature_npz(f'resultados/{nome}.npz',f'resultados/{nome}_FeatVec.npz')
 
 #%%
 # inicializando extrator de características
@@ -35,6 +45,9 @@ while(1):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # procurando rosto no vídeo com haar cascade
     faces = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+    print(f'Rostos encontrados: {len(faces)}') # Imprimi a quantidade de rostos encontrados
+
     # bounding box
     for (x, y, w, h) in faces:          
         # desenhando bb e recortando rosto
@@ -42,14 +55,18 @@ while(1):
         X2, Y2 = int(x+w), int(y+h)
 
         rosto = frame[Y1:Y2, X1:X2] # recorta o rosto
-        cv2.rectangle(frame, (X1, Y1), (X2, Y2), (255, 0 , 0))
+        cv2.rectangle(frame, (X1, Y1), (X2, Y2), (255, 0 , 0)) # desenha Boundingbox
         
-        rosto = EC.preprocessamento(rosto)
-        ftvec = EC.extrair_caracteristicas(rosto)
+        rosto = EC.preprocessamento(rosto) # processa rosto para extração de carcteristicas
+        ftvec = EC.extrair_caracteristicas(rosto) # extrai as caracteristicas
 
-        pessoa = reconhecedor.prever(ftvec)
-        cv2.putText(frame, f'{pessoa[0]}',(X1,Y2+10), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0))
-    
+        pessoa = reconhecedor.prever(ftvec) # "adivinha" que é
+        cv2.rectangle(frame, (X1, Y2), (X2,Y2+20), (255, 0, 0), -1) # retangulo preenchido
+        cv2.putText(frame, f'{pessoa[0]}',(X1+15,Y2+15), cv2.FONT_HERSHEY_PLAIN, 1, (255,250,250))
+
+        print(f'{pessoa[0]}') # imprime o nome da pessoa
+    print('')
+
     cv2.imshow("Video", frame)
 
     k = cv2.waitKey(30) & 0xff
