312aeb2356547add3dd43c06019b93ed977862a4
Juan <juanjpnv@hotmail.com>
2019-12-15 23:04:41 -0300

    Adicionada função para teste SKfold

--- classificar.py
+++ classificar.py
@@ -5,6 +5,8 @@
 #%%
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score, f1_score, precision_score
+from sklearn.model_selection import StratifiedKFold
 
 #%%
 class Reconhecer:
@@ -42,3 +44,37 @@ class Reconhecer:
             X = feature vector da imagem a ser prevista
         '''
         return self.svm.predict(X)
+
+def teste_skf(path):
+    data = np.load(path, allow_pickle=1)
+    X, y = data['arr_0'].reshape(-1,128), data['arr_1']
+    skf = StratifiedKFold(n_splits=5)
+    svc = LinearSVC()
+    acuracia = []
+    precisao = []
+    f1_lista = []
+
+    for train_index, test_index in skf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        svc.fit(X_train, y_train)
+        y_pred = svc.predict(X_test)
+
+        ac = accuracy_score(y_test, y_pred)
+        acuracia.append(ac)
+        print(f'Acurácia: {ac}')
+
+        pre = precision_score(y_test, y_pred, average='macro')
+        precisao.append(pre)
+        print(f'Precisão: {pre}')
+
+        f1 = f1_score(y_test, y_pred, average='macro')
+        f1_lista.append(f1)
+        print(f'F1 Score: {f1}')
+
+        print('')
+
+    print(f'Acurácia média: {np.array(acuracia).mean()}')
+    print(f'Precisão média: {np.array(precisao).mean()}')
+    print(f'F1 Score médio: {np.array(f1_lista).mean()}')
